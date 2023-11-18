bb92c06a59f0995b9e0833a2773cc7d2a151334e
krisztian_karolyi <xkiki2001@gmail.com>
2023-11-18 23:40:09 +0200

    gui improvement

--- chat/src/chatGUI.java
+++ chat/src/chatGUI.java
@@ -1,104 +1,128 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.json.*;
-
+import net.miginfocom.swing.MigLayout;
 
 public class chatGUI  extends JFrame{
+
+    private DefaultListModel<String> userListModel;
+    private JList<String> userList;
+
+
     private String userName = "";
     private JTextField messageField;
     private JTextArea chatArea;
     private Socket clientSocket;
+    private Font font = new Font("MONOSPACED", Font.PLAIN, 18);
 
 
     public chatGUI(String host, int port) {
         initUI();
         startClient(host, port);
+        SwingUtilities.invokeLater(this::requestUserList);
     }
 
     private void initUI() {
         setTitle("Chat Client");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500, 400);
+        setSize(800, 600);
         setLocationRelativeTo(null);
+        getContentPane().setBackground(Color.DARK_GRAY);
+        getContentPane().setForeground(Color.WHITE);
+        getContentPane().setFont(font);
 
         messageField = new JTextField();
         chatArea = new JTextArea();
         chatArea.setEditable(false);
 
+        messageField.setBackground(Color.DARK_GRAY);
+        chatArea.setBackground(Color.DARK_GRAY);
+        messageField.setForeground(Color.WHITE);
+        chatArea.setForeground(Color.WHITE);
+        chatArea.setFont(font);
+        messageField.setFont(font);
+
         JButton sendButton = new JButton("Send");
         sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String message = messageField.getText().toString();
-                if(message.toLowerCase().startsWith("@private"))
-                {
-                    String[] parts = message.split(" ", 4);
-
-                    if (parts.length == 4) {
-                        String receiver = parts[1];
-                        String content = parts[3];
-                        appendToChatArea(String.format("[Privát]: Te --> %s: %s", receiver, content));
-                        sendMessage(clientSocket, "private", content, userName, receiver);
-                    }
-                    else {
-                        appendToChatArea("Hibás privát üzenet formátum. Használd a következőt: @private [címzett] -> [üzenet]");
-                    }
-                }
-
-                else if(message.toLowerCase().equals("@help")){
-                    printAvailableCommands();
-                }
-
-                else {
-                    sendMessage(clientSocket, "public", message, userName, "all");
-                }
-
-                messageField.setText("");
+                sendMessageFromTextField();
             }
         });
 
+        messageField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    sendMessageFromTextField();
+                }
+            }
+        });
 
+        userListModel = new DefaultListModel<>();
+        userList = new JList<>(userListModel);
+        userList.setBackground(Color.DARK_GRAY);
+        userList.setForeground(Color.white);
+        userList.setFont(font);
 
-        JScrollPane scrollPane = new JScrollPane(chatArea);
+        JScrollPane userListScrollPane = new JScrollPane(userList);
 
-        GroupLayout layout = new GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setAutoCreateGaps(true);
-        layout.setAutoCreateContainerGaps(true);
+        JButton refreshButton = new JButton("Refresh");
+        refreshButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                requestUserList();
+            }
+        });
 
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addComponent(scrollPane)
-                        .addGroup(layout.createSequentialGroup()
-                                .addComponent(messageField)
-                                .addComponent(sendButton))
-        );
+        userList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 1) {
+                    JList<String> list = (JList<String>) e.getSource();
+                    String selectedUser = list.getSelectedValue();
+                    if (selectedUser != null) {
+                        messageField.setText(String.format("@private %s -> ", (selectedUser).trim()));
+                    }
+                }
+            }
+        });
 
-        layout.setVerticalGroup(
-                layout.createSequentialGroup()
-                        .addComponent(scrollPane)
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(messageField)
-                                .addComponent(sendButton))
-        );
+        setLayout(new MigLayout("fill", "[75%][25%]", "[75%, grow][25%, grow]"));
 
+        add(new JScrollPane(chatArea), "cell 0 0, grow");
+        add(userListScrollPane, "cell 1 0, grow");
+        add(messageField, "cell 0 1, grow");
+        add(sendButton, "cell 1 1, grow");
+        add(refreshButton, "cell 1 1, grow");
 
+    }
+
+    private  void requestUserList() {
+        sendMessage(clientSocket, "server", "@users", userName, "server");
+    }
+
+    private void updateUsersList(String users) {
+        SwingUtilities.invokeLater(() -> {
+            userListModel.clear();
+            String[] userArray = users.split(":")[1].split(",");
+            for (String user : userArray) {
+                userListModel.addElement(user);
+            }
+        });
     }
 
     private void startClient(String host, int port) {
         try {
             clientSocket = new Socket(host, port);
 
-            // Prompt the user for their name using a dialog
-
-            while(userName.equals(""))
+            while(userName.replace(" ", "").equals(""))
             {
                 userName = JOptionPane.showInputDialog(this, "Kérjük, adja meg a nevét:", "Bejelentkezés", JOptionPane.PLAIN_MESSAGE);
             }
@@ -109,7 +133,7 @@ public class chatGUI  extends JFrame{
 
 
         } catch (IOException e) {
-            appendToChatArea("A kapcsolat megszakadt.");
+            appendToChatArea("A kapcsolat megszakadt / sikertelen kapcsolódás.");
             e.printStackTrace();
         }
     }
@@ -124,31 +148,27 @@ public class chatGUI  extends JFrame{
                 int bytesRead = inputStream.read(buffer);
 
                 if (bytesRead == -1) {
-                    // The connection is lost
-                    appendToChatArea("The connection is lost.");
+                    appendToChatArea("A kapcsolat megszakadt / sikertelen kapcsolódás.");
                     break;
                 }
 
                 String jsonMessage = new String(buffer, 0, bytesRead, StandardCharsets.UTF_8);
 
                 if ("@exit".equals(jsonMessage.trim())) {
-                    appendToChatArea("The connection is lost.");
+                    appendToChatArea("Kiléptél a chatből.");
+                    System.exit(0);
                     break;
-
                 }
-
-                // Update GUI with received message
                 SwingUtilities.invokeLater(() -> processReceivedJsonMessage(jsonMessage));
             }
         } catch (IOException e) {
-            appendToChatArea("Error receiving messages.");
+            appendToChatArea("Hiba az üzenetek fogadása közben.");
             e.printStackTrace();
         }
     }
 
     private void processReceivedJsonMessage(String jsonMessage) {
         try (JsonReader reader = Json.createReader(new StringReader(jsonMessage))) {
-            // JSON object creation from the received text
             JsonObject jsonObject = reader.readObject();
             String messageType = jsonObject.getString("message_type");
             String sender = jsonObject.getString("sender");
@@ -156,14 +176,16 @@ public class chatGUI  extends JFrame{
             String time = jsonObject.getString("timestamp");
 
             if ("server".equals(messageType)) {
-                // Server message
-                appendToChatArea("[SZERVER] | " + time + ": " + content  );
+                if (content.contains("felhasználók")) {
+                    updateUsersList(content);
+                }
+                else {
+                    appendToChatArea("[SZERVER] | " + time + ": " + content);
+                }
             } else if ("public".equals(messageType)) {
-                // Public message
                 appendToChatArea("<" + sender + "> | " + time + ": " + content);
             } else if ("private".equals(messageType)) {
-                // Private message
-                appendToChatArea("[Privát] küldte: <" + sender + "> | " + time + ": " + content );
+                appendToChatArea("[Privát] küldte: <" + sender + "> | " + time + ": " + content);
             }
         } catch (Exception e) {
             appendToChatArea("Hiba a JSON üzenet feldolgozása során.");
@@ -172,19 +194,49 @@ public class chatGUI  extends JFrame{
     }
 
 
-    private String readLine(BufferedReader reader) throws IOException {
+    private void sendMessageFromTextField() {
+        String message = messageField.getText().toString();
+        if(message.toLowerCase().startsWith("@private"))
+        {
+            String[] parts = message.split(" ", 4);
+
+            if (parts.length == 4) {
+                String receiver = parts[1];
+                String content = parts[3];
+                appendToChatArea(String.format("[Privát]: Te --> %s: %s", receiver, content));
+                sendMessage(clientSocket, "private", content, userName, receiver);
+            }
+            else {
+                appendToChatArea("Hibás privát üzenet formátum. Használd a következőt: @private [címzett] -> [üzenet]");
+            }
+        }
+
+        else if(message.toLowerCase().equals("@help")){
+            printAvailableCommands();
+        }
+
+        else {
+            sendMessage(clientSocket, "public", message, userName, "all");
+        }
+
+        messageField.setText("");
+
+    }
+
+    private String readLine(BufferedReader reader) throws IOException
+    {
         return reader.readLine();
     }
 
-    public  void printAvailableCommands() {
+    public  void printAvailableCommands()
+    {
         appendToChatArea("Elérhető parancsok:");
         appendToChatArea("@exit - Kilépés a chatről+ \n @help - Elérhető parancsok listázása \n @users - Aktív felhasználók listázása \n \"@newName [új név] - Felhasználónév megváltoztatása\"");
     }
-    // Add this method to append messages to the chatArea
+
     private void appendToChatArea(String message) {
         chatArea.append(message + "\n");
     }
-
 
     private void sendMessage(Socket clientSocket, String messageType, String content, String sender, String receiver) {
         try {
@@ -194,8 +246,6 @@ public class chatGUI  extends JFrame{
 
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             out.println(message);
-
-            // Clear the messageField after sending the message
             messageField.setText("");
 
         } catch (IOException e) {
@@ -211,10 +261,10 @@ public class chatGUI  extends JFrame{
                 String ip = JOptionPane.showInputDialog("Kérem adja meg az IP-címet:");
                 String portStr = JOptionPane.showInputDialog("Kérem adja meg a portot:");
                 int port = Integer.parseInt(portStr);
-
                 new chatGUI(ip, port).setVisible(true);
             }
         });
     }
+
 
 }
